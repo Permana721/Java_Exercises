ff46fb3e7a3f2fbb120cf359c2c685d2312cb05d
PermanaSD <sdharma721@gmail.com>
2026-07-23 10:11:21 +0700

    feat: add Todolist class with basic properties and constructor

--- src/_02_Todolist_JavaDasar/Todolist.java
+++ src/_02_Todolist_JavaDasar/Todolist.java
@@ -1,4 +1,20 @@
 package _02_Todolist_JavaDasar;
 
 public class Todolist {
+    private int id = 0;
+    private String judul;
+
+    public Todolist(String judul) {
+        this.id++;
+        this.judul = judul;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
 }
