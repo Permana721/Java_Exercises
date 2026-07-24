a4566b4e37ce9e374ee7cec2a969363fac599083
PermanaSD <sdharma721@gmail.com>
2026-07-24 16:23:44 +0700

    refactor: add dependency injection and service layer in Main.java

--- src/_3_Todolist_JavaOOP/Main.java
+++ src/_3_Todolist_JavaOOP/Main.java
@@ -1,7 +1,17 @@
 package _3_Todolist_JavaOOP;
 
+import _3_Todolist_JavaOOP.repository.TodoListRepository;
+import _3_Todolist_JavaOOP.repository.TodoListRepositoryImpl;
+import _3_Todolist_JavaOOP.service.TodoListService;
+import _3_Todolist_JavaOOP.service.TodoListServiceImpl;
+import _3_Todolist_JavaOOP.view.TodoListView;
+
 public class Main {
     static void main(String[] args) {
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
 
+        todoListView.showTodoList();
     }
 }
