85bb042c923fb7d7de9d70e5cae1d65c37713132
PermanaSD <sdharma721@gmail.com>
2026-07-24 13:34:15 +0700

    chore: update TodoListView.java

--- src/_3_Todolist_JavaOOP/view/TodoListView.java
+++ src/_3_Todolist_JavaOOP/view/TodoListView.java
@@ -1,5 +1,15 @@
 package _3_Todolist_JavaOOP.view;
 
 public class TodoListView {
+    public void showTodoList(){
 
+    }
+
+    public void addTodoList(){
+
+    }
+
+    public void removeTodoList(){
+
+    }
 }
