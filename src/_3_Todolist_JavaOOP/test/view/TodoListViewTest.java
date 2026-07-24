c957c29d9d94908af355e16a15acdaad12aca1bc
PermanaSD <sdharma721@gmail.com>
2026-07-24 16:23:44 +0700

    chore: update TodoListViewTest.java

--- src/_3_Todolist_JavaOOP/test/view/TodoListViewTest.java
+++ src/_3_Todolist_JavaOOP/test/view/TodoListViewTest.java
@@ -1,4 +1,33 @@
 package _3_Todolist_JavaOOP.test.view;
 
+import _3_Todolist_JavaOOP.repository.TodoListRepository;
+import _3_Todolist_JavaOOP.repository.TodoListRepositoryImpl;
+import _3_Todolist_JavaOOP.service.TodoListService;
+import _3_Todolist_JavaOOP.service.TodoListServiceImpl;
+import _3_Todolist_JavaOOP.view.TodoListView;
+
 public class TodoListViewTest {
+    static void main(String[] args) {
+        testAddTodoList();
+    }
+
+    public static void testShowTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListService.addTodoList("Belajar Java Dasar");
+        todoListService.addTodoList("Belajar Java OOP");
+        todoListService.addTodoList("Belajar Java Collection");
+
+        todoListView.showTodoList();
+    }
+
+    public static void testAddTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListView.addTodoList();
+    }
 }
