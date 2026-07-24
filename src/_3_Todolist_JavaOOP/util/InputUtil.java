84f56362d2a5e2b21758a226271f4dd2d5247afb
PermanaSD <sdharma721@gmail.com>
2026-07-24 16:23:44 +0700

    feat: add input utility methods to InputUtil class

--- src/_3_Todolist_JavaOOP/util/InputUtil.java
+++ src/_3_Todolist_JavaOOP/util/InputUtil.java
@@ -1,4 +1,14 @@
 package _3_Todolist_JavaOOP.util;
 
+import java.util.Scanner;
+
 public class InputUtil {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String input(String info) {
+        System.out.print(info + " ");
+        String data = scanner.nextLine();
+        return data;
+    }
 }
