fb6b2e4ff5adf51900f126603e800c250bd65c6f
PermanaSD <sdharma721@gmail.com>
2026-07-25 16:55:28 +0700

    feat: add test file for Product class

--- src/_04_ECommerce/test/TestProduct.java
+++ src/_04_ECommerce/test/TestProduct.java
@@ -1,4 +1,12 @@
 package _04_ECommerce.test;
 
+import _04_ECommerce.entity.Category;
+import _04_ECommerce.entity.Product;
+
 public class TestProduct {
+    static void main(String[] args) {
+        Product product = new Product("Jam", Category.BEVERAGE, 30000);
+
+        product.getDetailProduct();
+    }
 }
