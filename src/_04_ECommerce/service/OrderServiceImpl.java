ed0f7be026cc051ab017df9235269bf74a0049e1
PermanaSD <sdharma721@gmail.com>
2026-07-25 14:37:30 +0700

    feat: add OrderServiceImpl class

--- src/_04_ECommerce/service/OrderServiceImpl.java
+++ src/_04_ECommerce/service/OrderServiceImpl.java
@@ -1,4 +1,27 @@
 package _04_ECommerce.service;
 
-public class OrderServiceImpl {
+import _04_ECommerce.entity.Order;
+
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public String checkout(String customerId) {
+        return "";
+    }
+
+    @Override
+    public Order processNextOrder() {
+        return null;
+    }
+
+    @Override
+    public List<Order> getOrderHistory(String customerId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        return List.of();
+    }
 }
