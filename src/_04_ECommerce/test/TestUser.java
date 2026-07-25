324bff62d917ccb5974dca5231ee6a230160b042
PermanaSD <sdharma721@gmail.com>
2026-07-25 16:55:28 +0700

    feat: add test file for User entity

--- src/_04_ECommerce/test/TestUser.java
+++ src/_04_ECommerce/test/TestUser.java
@@ -1,4 +1,13 @@
 package _04_ECommerce.test;
 
+import _04_ECommerce.entity.User;
+
 public class TestUser {
+    static void main(String[] args) {
+        User user = new User("Permana", null, null);
+        User user1 = new User("Arido", null, null);
+
+        user.getDetailUser();
+        user1.getDetailUser();
+    }
 }
