bcbaa1be079b0350a1cd14d78b17d2567871b00d
PermanaSD <sdharma721@gmail.com>
2026-07-25 14:37:30 +0700

    feat: add UserRepositoryImpl class for user repository implementation

--- src/_04_ECommerce/repository/UserRepositoryImpl.java
+++ src/_04_ECommerce/repository/UserRepositoryImpl.java
@@ -1,4 +1,22 @@
 package _04_ECommerce.repository;
 
-public class UserRepositoryImpl {
+import _04_ECommerce.entity.User;
+
+import java.util.List;
+
+public class UserRepositoryImpl implements UserRepository {
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public User findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return List.of();
+    }
 }
