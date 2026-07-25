65d58bd078f877bc0049b4cd5a5f79104d3de5ba
PermanaSD <sdharma721@gmail.com>
2026-07-25 14:37:30 +0700

    feat: add initial implementation of ProductRepositoryImpl

--- src/_04_ECommerce/repository/ProductRepositoryImpl.java
+++ src/_04_ECommerce/repository/ProductRepositoryImpl.java
@@ -1,4 +1,22 @@
 package _04_ECommerce.repository;
 
-public class ProductRepositoryImpl {
+import _04_ECommerce.entity.Product;
+
+import java.util.List;
+
+public class ProductRepositoryImpl implements ProductRepository{
+    @Override
+    public Product save(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return List.of();
+    }
 }
