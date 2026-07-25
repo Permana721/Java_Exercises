f8a37c24869c6379012d28abfce9931001134bbc
PermanaSD <sdharma721@gmail.com>
2026-07-25 16:55:28 +0700

    chore: update ProductRepositoryImpl.java

--- src/_04_ECommerce/repository/ProductRepositoryImpl.java
+++ src/_04_ECommerce/repository/ProductRepositoryImpl.java
@@ -1,17 +1,33 @@
 package _04_ECommerce.repository;
 
 import _04_ECommerce.entity.Product;
+import _04_ECommerce.exception.UserNotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepositoryImpl implements ProductRepository{
+    private final List<Product> products;
+
+    public ProductRepositoryImpl(List<Product> products) {
+        this.products = products;
+    }
+
     @Override
     public Product save(Product product) {
-        return null;
+        products.add(product);
+        return product;
     }
 
     @Override
     public Product findById(String id) {
+        for (Product product : products){
+            if (product.getId().equals(id)){
+                return product;
+            } else {
+                throw new UserNotFoundException("User with Id " + id + " not found!");
+            }
+        }
         return null;
     }
 
