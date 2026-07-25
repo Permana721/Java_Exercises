c7d1916871bf0d816b7324fded1c317eea32affd
PermanaSD <sdharma721@gmail.com>
2026-07-25 14:37:30 +0700

    chore: update OrderService.java

--- src/_04_ECommerce/service/OrderService.java
+++ src/_04_ECommerce/service/OrderService.java
@@ -1,4 +1,12 @@
 package _04_ECommerce.service;
 
+import _04_ECommerce.entity.Order;
+
+import java.util.List;
+
 public interface OrderService {
+    String checkout(String customerId);
+    Order processNextOrder();
+    List<Order> getOrderHistory(String customerId);
+    List<Order> getAllOrders();
 }
