2cc936ca29e8d3acbcc88a512646c1f82b0fe2fb
PermanaSD <sdharma721@gmail.com>
2026-07-25 14:37:30 +0700

    chore: update CartServiceImpl.java

--- src/_04_ECommerce/service/CartServiceImpl.java
+++ src/_04_ECommerce/service/CartServiceImpl.java
@@ -1,4 +1,15 @@
 package _04_ECommerce.service;
 
-public class CartServiceImpl {
+import _04_ECommerce.entity.Cart;
+
+public class CartServiceImpl implements CartService {
+    @Override
+    public Cart add(Cart cart) {
+        return null;
+    }
+
+    @Override
+    public Cart remove(Cart cart) {
+        return null;
+    }
 }
