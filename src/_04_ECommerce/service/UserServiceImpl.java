a4d052c1c961a87183f573c884d820a1fdde7eac
PermanaSD <sdharma721@gmail.com>
2026-07-26 11:34:08 +0700

    feat: add UserServiceImpl class to handle user operations

--- src/_04_ECommerce/service/UserServiceImpl.java
+++ src/_04_ECommerce/service/UserServiceImpl.java
@@ -1,4 +1,40 @@
 package _04_ECommerce.service;
 
-public class UserServiceImpl {
+import _04_ECommerce.entity.User;
+import _04_ECommerce.exception.UserNotFoundException;
+import _04_ECommerce.repository.UserRepository;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    UserRepository users;
+
+    public UserServiceImpl(UserRepository users) {
+        this.users = users;
+    }
+
+    @Override
+    public User createUser(User user) {
+        return users.save(user);
+    }
+
+    @Override
+    public User selectUser(String id) {
+        User user = users.findById(id);
+        if (user.getId().equals(id)) {
+            return user;
+        } else {
+            throw new UserNotFoundException("User with Id: " + id + " not found!");
+        }
+    }
+
+    @Override
+    public List<User> selectAll() {
+        List<User> users1 = users.findAll();
+
+        for (User user : users1){
+            user.getDetailUser();
+        }
+        return null;
+    }
 }
