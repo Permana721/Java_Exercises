0cc88ffb83f5fef3bc5b3c904adfa0fdab6c05ea
PermanaSD <sdharma721@gmail.com>
2026-07-26 11:34:08 +0700

    feat: add user role and update service usage in TestUser.java

--- src/_04_ECommerce/test/TestUser.java
+++ src/_04_ECommerce/test/TestUser.java
@@ -1,13 +1,22 @@
 package _04_ECommerce.test;
 
 import _04_ECommerce.entity.User;
+import _04_ECommerce.entity.UserRole;
+import _04_ECommerce.repository.UserRepository;
+import _04_ECommerce.repository.UserRepositoryImpl;
+import _04_ECommerce.service.UserService;
+import _04_ECommerce.service.UserServiceImpl;
 
 public class TestUser {
     static void main(String[] args) {
-        User user = new User("Permana", null, null);
-        User user1 = new User("Arido", null, null);
+        UserRepository userRepository = new UserRepositoryImpl();
+        UserService userService = new UserServiceImpl(userRepository);
 
-        user.getDetailUser();
-        user1.getDetailUser();
+        User user = new User("Permana", null, null, UserRole.USER);
+        User user1 = new User("Arido", null, null, UserRole.ADMIN);
+
+        userService.createUser(user);
+        userService.createUser(user1);
+        userService.selectAll();
     }
 }
