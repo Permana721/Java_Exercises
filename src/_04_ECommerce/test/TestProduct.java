c3e47682cd2616a48472bd6461d9528d54c6188a
PermanaSD <sdharma721@gmail.com>
2026-07-26 11:34:08 +0700

    feat: add product repository and service usage in TestProduct.java

--- src/_04_ECommerce/test/TestProduct.java
+++ src/_04_ECommerce/test/TestProduct.java
@@ -2,11 +2,23 @@ package _04_ECommerce.test;
 
 import _04_ECommerce.entity.Category;
 import _04_ECommerce.entity.Product;
+import _04_ECommerce.repository.ProductRepository;
+import _04_ECommerce.repository.ProductRepositoryImpl;
+import _04_ECommerce.service.ProductService;
+import _04_ECommerce.service.ProductServiceImpl;
 
 public class TestProduct {
     static void main(String[] args) {
-        Product product = new Product("Jam", Category.BEVERAGE, 30000);
+        ProductRepository productRepository = new ProductRepositoryImpl();
+        ProductService productService = new ProductServiceImpl(productRepository);
 
-        product.getDetailProduct();
+
+        Product product = new Product("Jam", Category.FURNITURE, 30000);
+        Product product2 = new Product("Jam Tangan", Category.ELECTRONIC, 150_000);
+
+
+        productService.add(product);
+        productService.add(product2);
+        productService.findAll();
     }
 }
