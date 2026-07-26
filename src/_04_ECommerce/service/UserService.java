df30ea29f2e5b4cd339f912bd4ebbf7f73b4ee25
PermanaSD <sdharma721@gmail.com>
2026-07-26 11:34:08 +0700

    feat: add UserService interface for ECommerce module

--- src/_04_ECommerce/service/UserService.java
+++ src/_04_ECommerce/service/UserService.java
@@ -1,4 +1,11 @@
 package _04_ECommerce.service;
 
+import _04_ECommerce.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+    User createUser(User user);
+    User selectUser(String id);
+    List<User> selectAll();
 }
