855949ab12de827780d698b5a7b40f2ab9e7d640
PermanaSD <sdharma721@gmail.com>
2026-07-26 11:34:08 +0700

    chore: update UserRepositoryImpl.java

--- src/_04_ECommerce/repository/UserRepositoryImpl.java
+++ src/_04_ECommerce/repository/UserRepositoryImpl.java
@@ -1,22 +1,38 @@
 package _04_ECommerce.repository;
 
 import _04_ECommerce.entity.User;
+import _04_ECommerce.exception.UserNotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepositoryImpl implements UserRepository {
+    private final List<User> users;
+
+    public UserRepositoryImpl() {
+        this.users = new ArrayList<>();
+    }
+
     @Override
     public User save(User user) {
-        return null;
+        users.add(user);
+        return user;
     }
 
     @Override
     public User findById(String id) {
+        for (User user : users){
+            if (user.getId().equals(id)){
+                return user;
+            } else {
+                throw new UserNotFoundException("User with Id: " + id + " not found!");
+            }
+        }
         return null;
     }
 
     @Override
     public List<User> findAll() {
-        return List.of();
+        return users;
     }
 }
