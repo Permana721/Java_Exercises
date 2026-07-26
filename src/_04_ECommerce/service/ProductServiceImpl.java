286a15d9c2b356f489b18e44dd7ad87d4d97c803
PermanaSD <sdharma721@gmail.com>
2026-07-26 11:34:08 +0700

    chore: update ProductServiceImpl.java

--- src/_04_ECommerce/service/ProductServiceImpl.java
+++ src/_04_ECommerce/service/ProductServiceImpl.java
@@ -1,15 +1,42 @@
 package _04_ECommerce.service;
 
 import _04_ECommerce.entity.Product;
+import _04_ECommerce.exception.ProductNotFoundException;
+import _04_ECommerce.repository.ProductRepository;
+
+import java.util.List;
+import java.util.Objects;
 
 public class ProductServiceImpl implements ProductService {
-    @Override
-    public Product add(Product product) {
-        return null;
+    private ProductRepository products;
+
+    public ProductServiceImpl(ProductRepository products) {
+        this.products = products;
     }
 
     @Override
-    public Product findById(String id) {
+    public Product add(Product product) {
+        return products.save(product);
+    }
+
+    @Override
+    public Product selectProduct(String id) {
+        Product product = products.findById(id);
+        if (Objects.isNull(product)){
+            throw new ProductNotFoundException("User with id " + id + " not found!");
+        } else {
+            return product;
+        }
+    }
+
+    @Override
+    public List<Product> findAll() {
+        List<Product> products1 =  products.findAll();
+
+        for (Product product : products1) {
+            product.getDetailProduct();
+        }
+
         return null;
     }
 }
