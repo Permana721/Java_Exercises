d2a53c9a4d75f52110f090beae59305a5f4c543b
PermanaSD <sdharma721@gmail.com>
2026-07-26 11:34:08 +0700

    refactor: add new categories and getter method in Category enum

--- src/_04_ECommerce/entity/Category.java
+++ src/_04_ECommerce/entity/Category.java
@@ -1,13 +1,19 @@
 package _04_ECommerce.entity;
 
 public enum Category {
-    DRINK("Drink"),
-    BEVERAGE("Beverage"),
+    FASHION("Fashion"),
+    ELECTRONIC("Electronic"),
+    HEALTHY("Healthy"),
+    FURNITURE("Furnitre"),
     FOOD("Food");
 
     String displayName;
 
     Category(String displayName) {
         this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 }
