e57d491e340d94a7a3fdfaefd8a802496a0bfe5d
Permana <sdharma721@gmail.com>
2026-07-30 19:19:41 +0700

    fix: update user creation and add login functionality

--- src/_04_ECommerce/test/TestUser.java
+++ src/_04_ECommerce/test/TestUser.java
@@ -1,25 +1,27 @@
 package _04_ECommerce.test;
 
-import _04_ECommerce.entity.User;
-import _04_ECommerce.entity.UserRole;
+import _04_ECommerce.entity.*;
+import _04_ECommerce.repository.ProductRepository;
+import _04_ECommerce.repository.ProductRepositoryImpl;
 import _04_ECommerce.repository.UserRepository;
 import _04_ECommerce.repository.UserRepositoryImpl;
+import _04_ECommerce.service.ProductService;
+import _04_ECommerce.service.ProductServiceImpl;
 import _04_ECommerce.service.UserService;
 import _04_ECommerce.service.UserServiceImpl;
-
-import java.util.Comparator;
-import java.util.Objects;
 
 public class TestUser {
     static void main(String[] args) {
         UserRepository userRepository = new UserRepositoryImpl();
         UserService userService = new UserServiceImpl(userRepository);
 
-        User user = new User("Permana", null, null, UserRole.USER);
-        User user1 = new User("Arido", null, null, UserRole.ADMIN);
+        User user = new User("Permana", "123" ,UserRole.USER);
+        User user1 = new User("Arido", "333" ,UserRole.ADMIN);
+
 
         userService.createUser(user);
         userService.createUser(user1);
         userService.selectAll();
+        System.out.println(userService.login("Permana", "123"));
     }
 }
