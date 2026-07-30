c0241726254d3ab4f19c0593f384f2e7d3bec46e
Permana <sdharma721@gmail.com>
2026-07-30 19:19:41 +0700

    chore: update Cart.java

--- src/_04_ECommerce/entity/Cart.java
+++ src/_04_ECommerce/entity/Cart.java
@@ -1,4 +1,24 @@
 package _04_ECommerce.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+    private List<CartItem> cartItems;
+
+    public Cart(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public Cart() {
+
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
 }
