5e39f83b8795216d27a031aa68311a3b618df86d
Permana <sdharma721@gmail.com>
2026-07-30 19:19:41 +0700

    chore: update Order.java

--- src/_04_ECommerce/entity/Order.java
+++ src/_04_ECommerce/entity/Order.java
@@ -1,5 +1,17 @@
 package _04_ECommerce.entity;
 
 public class Order {
+    private Cart cart;
 
+    public Order(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
 }
