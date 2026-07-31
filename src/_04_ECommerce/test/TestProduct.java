e43cdf684afb8f984ca58dc735343c1259cba7f8
Permana <sdharma721@gmail.com>
2026-07-31 11:46:26 +0700

    chore: comment out unused code in TestProduct.java

--- src/_04_ECommerce/test/TestProduct.java
+++ src/_04_ECommerce/test/TestProduct.java
@@ -9,16 +9,16 @@ import _04_ECommerce.service.ProductServiceImpl;
 
 public class TestProduct {
     static void main(String[] args) {
-        ProductRepository productRepository = new ProductRepositoryImpl();
-        ProductService productService = new ProductServiceImpl(productRepository);
-
-
-        Product product = new Product("Jam", Category.FURNITURE, 30000);
-        Product product2 = new Product("Jam Tangan", Category.ELECTRONIC, 150_000);
-
-
-        productService.add(product);
-        productService.add(product2);
-        productService.findAll();
+//        ProductRepository productRepository = new ProductRepositoryImpl();
+//        ProductService productService = new ProductServiceImpl(productRepository);
+//
+//
+//        Product product = new Product("Jam", Category.FURNITURE, 30000, 5);
+//        Product product2 = new Product("Jam Tangan", Category.ELECTRONIC, 150_000, 7);
+//
+//
+//        productService.add(product);
+//        productService.add(product2);
+//        productService.findAll();
     }
 }
