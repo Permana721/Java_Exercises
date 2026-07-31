8d79ad6cf92bcb33e2d0af4562d67bc949247bc5
Permana <sdharma721@gmail.com>
2026-07-31 11:46:26 +0700

    feat: add InputUtil class for user input handling

--- src/_04_ECommerce/util/InputUtil.java
+++ src/_04_ECommerce/util/InputUtil.java
@@ -1,4 +1,27 @@
 package _04_ECommerce.util;
 
+import java.util.Scanner;
+
 public class InputUtil {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String input(String info) {
+        System.out.print(info + " ");
+        String data = scanner.nextLine();
+        return data;
+    }
+
+    public static double aDouble(String info){
+        System.out.println(info + " ");
+        double data = scanner.nextDouble();
+        scanner.nextLine();
+        return data;
+    }
+
+    public static int anInt(String info){
+        System.out.println(info + " ");
+        int data = scanner.nextInt();
+        scanner.nextLine();
+        return data;
+    }
 }
