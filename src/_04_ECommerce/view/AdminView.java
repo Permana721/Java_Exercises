ddac201f9203d6da494be7502bf5367ebe2e40b1
Permana <sdharma721@gmail.com>
2026-07-31 14:56:38 +0700

    chore: update AdminView.java

--- src/_04_ECommerce/view/AdminView.java
+++ src/_04_ECommerce/view/AdminView.java
@@ -3,19 +3,20 @@ package _04_ECommerce.view;
 import _04_ECommerce.entity.Category;
 import _04_ECommerce.entity.Product;
 import _04_ECommerce.entity.User;
+import _04_ECommerce.exception.ProductNotNullException;
 import _04_ECommerce.service.ProductService;
+import _04_ECommerce.service.ProductServiceImpl;
 import _04_ECommerce.service.UserService;
 import _04_ECommerce.util.InputUtil;
 
+import java.util.Objects;
+
 public class AdminView {
     private UserService userService;
-    private ProductService productService;
+    private final ProductService productService;
 
-    public AdminView() {
-    }
-
-    public AdminView(User user) {
-        this.userService = userService;
+    public AdminView(ProductService productService) {
+        this.productService = productService;
     }
 
     public void showMenu(){
@@ -34,9 +35,51 @@ public class AdminView {
 
             switch (input){
                 case "1" -> addProduct();
-                case "2" -> {}
-                case "3" -> {}
-                case "4" -> {}
+                case "2" -> {
+                    String id = InputUtil.input("Masukkan ID produk yang ingin di edit: ");
+                    Product product = productService.selectProduct(id);
+                    if (Objects.isNull(product)){
+                        throw new ProductNotNullException("Product cannot set to be NULL!");
+                    }
+                    product.getDetailProduct();
+                    String pilihan = InputUtil.input("Bagian apa yang ingin di edit dari produk di atas?\n1. Update nama\n2. Update harga\n 3. Update kategori\n4. Update stok\n0. Kembali\nPilih: ");
+                    switch (pilihan) {
+                        case "1" -> {
+                            String name = InputUtil.input("Masukkan nama baru: ");
+                            product.setName(name);
+                        }
+                        case "2" -> {
+                            String harga = InputUtil.input("Masukkan harga baru: ");
+                            product.setPrice(Integer.parseInt(harga));
+                        }
+                        case "3" -> {
+                            String categoryInput = InputUtil.input("Masukkan kategori baru\n1. Fashion\n2. Electronic\n3. Healthy \n4. Furniture\n5. Food\nPilih: ");
+                            Category category = null;
+                            switch (categoryInput) {
+                                case "1" -> category = Category.FASHION;
+                                case "2" -> category = Category.ELECTRONIC;
+                                case "3" -> category = Category.HEALTHY;
+                                case "4" -> category = Category.FURNITURE;
+                                case "5" -> category = Category.FOOD;
+                                default -> System.out.println("Tolong masukkan input yang valid!");
+
+                            }
+                            product.setCategory(category);
+                        }
+                        case "4" -> {
+                            String stok = InputUtil.input("Masukkan stok baru: ");
+                            product.setStock(Integer.parseInt(stok));
+                        }
+                        case "0" -> {
+                            return;
+                        }
+                    }
+                }
+                case "3" -> {
+                    String id = InputUtil.input("Masukan ID produk yang ingin dihapus: ");
+                    productService.deleteProduct(id);
+                }
+                case "4" -> productService.findAll();
                 case "5" -> {}
                 case "0" -> {
                     return;
@@ -47,22 +90,22 @@ public class AdminView {
 
     public void addProduct() {
         String name = InputUtil.input("Masukkan nama produk: ");
-        double price = InputUtil.aDouble("Masukkan harga produk: ");
-        int categoryInput = InputUtil.anInt("Masukkan kategori produk" +
-                "\n1. Fashion\n2. Electronic\n3. Healthy \n4. Furniture\n5. Food");
+        String price = InputUtil.input("Masukkan harga produk: ");
+        String categoryInput = InputUtil.input("Masukkan kategori produk" +
+                "\n1. Fashion\n2. Electronic\n3. Healthy \n4. Furniture\n5. Food\nPilih: ");
         Category category = null;
-        int stock = InputUtil.anInt("Masukkan stok produk: ");
+        String stock = InputUtil.input("Masukkan stok produk: ");
 
         switch (categoryInput) {
-            case 1 -> category = Category.FASHION;
-            case 2 -> category = Category.ELECTRONIC;
-            case 3 -> category = Category.HEALTHY;
-            case 4 -> category = Category.FURNITURE;
-            case 5 -> category = Category.FOOD;
+            case "1" -> category = Category.FASHION;
+            case "2" -> category = Category.ELECTRONIC;
+            case "3" -> category = Category.HEALTHY;
+            case "4" -> category = Category.FURNITURE;
+            case "5" -> category = Category.FOOD;
             default -> System.out.println("Tolong masukkan input yang valid!");
         }
 
-        Product newProduct = new Product(name, price, category, stock);
+        Product newProduct = new Product(name, Double.parseDouble(price), category, Integer.parseInt(stock));
         productService.add(newProduct);
     }
 }
