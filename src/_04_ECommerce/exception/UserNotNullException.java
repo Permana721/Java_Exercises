543f5485d55aef8e8194bca81b317452196fc5a5
Permana <sdharma721@gmail.com>
2026-07-31 14:56:38 +0700

    feat: add UserNotNullException class for handling user null errors

--- src/_04_ECommerce/exception/UserNotNullException.java
+++ src/_04_ECommerce/exception/UserNotNullException.java
@@ -1,7 +1,7 @@
 package _04_ECommerce.exception;
 
 public class UserNotNullException extends RuntimeException {
-  public UserNotNullException(String message) {
-    super(message);
-  }
+    public UserNotNullException(String message) {
+        super(message);
+    }
 }
