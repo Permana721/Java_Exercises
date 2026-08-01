3044a526e69189c236ba7681b6644a8f2701e718
Permana <sdharma721@gmail.com>
2026-08-01 15:02:16 +0700

    chore: update Cart.java

--- src/_04_ECommerce/entity/Cart.java
+++ src/_04_ECommerce/entity/Cart.java
@@ -1,24 +1,55 @@
 package _04_ECommerce.entity;
 
+import _04_ECommerce.exception.ProductNotFoundException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
     private List<CartItem> cartItems;
 
-    public Cart(List<CartItem> cartItems) {
-        this.cartItems = cartItems;
-    }
-
     public Cart() {
-
+        this.cartItems = new ArrayList<>();
     }
 
     public List<CartItem> getCartItems() {
         return cartItems;
     }
 
-    public void setCartItems(List<CartItem> cartItems) {
-        this.cartItems = cartItems;
+    public boolean addProduct(Product product, int qty) {
+        if (qty <= 0) {
+            throw new ProductNotFoundException("Tidak boleh membeli kurang dari 0!");
+        }
+        for (CartItem cartItem : cartItems) {
+            if (cartItem.getProduct().equals(product)){
+                cartItem.increaseQuantity(qty);
+                return true;
+            }
+        }
+        CartItem newCartItem = new CartItem(product, qty);
+        if (product.getStock() < qty){
+            System.out.println("Stok hanya tersedia " + product.getStock() + " saja");
+        } else {
+            cartItems.add(newCartItem);
+            return true;
+        }
+        return false;
     }
+
+    public void removeProduct(String productId) {
+
+    }
+
+    public void clear(){
+        cartItems.clear();
+    }
+
+    public void calculateTotal(){
+
+    }
+
+    public void isEmpty() {
+
+    }
+
 }
