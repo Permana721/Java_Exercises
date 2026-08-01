7dbe43ff301479a3933e31fc36d776cd2dc171c3
Permana <sdharma721@gmail.com>
2026-08-01 21:01:19 +0700

    chore: update OrderServiceImpl.java

--- src/_04_ECommerce/service/OrderServiceImpl.java
+++ src/_04_ECommerce/service/OrderServiceImpl.java
@@ -1,27 +1,13 @@
 package _04_ECommerce.service;
 
 import _04_ECommerce.entity.Order;
+import _04_ECommerce.entity.User;
 
 import java.util.List;
 
 public class OrderServiceImpl implements OrderService {
     @Override
-    public String checkout(String customerId) {
-        return "";
-    }
-
-    @Override
-    public Order processNextOrder() {
-        return null;
-    }
-
-    @Override
-    public List<Order> getOrderHistory(String customerId) {
-        return List.of();
-    }
-
-    @Override
-    public List<Order> getAllOrders() {
-        return List.of();
+    public boolean checkout(User user) {
+        return false;
     }
 }
