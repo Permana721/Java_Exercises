f20c41f8fb0057499c45a340ae49e76c4c7e4d5d
Permana <sdharma721@gmail.com>
2026-08-01 15:02:16 +0700

    refactor: remove unused code from TestProduct.java

--- src/_04_ECommerce/test/TestProduct.java
+++ src/_04_ECommerce/test/TestProduct.java
@@ -1,24 +1,9 @@
 package _04_ECommerce.test;
 
-import _04_ECommerce.entity.Category;
-import _04_ECommerce.entity.Product;
-import _04_ECommerce.repository.ProductRepository;
-import _04_ECommerce.repository.ProductRepositoryImpl;
-import _04_ECommerce.service.ProductService;
-import _04_ECommerce.service.ProductServiceImpl;
+
 
 public class TestProduct {
     static void main(String[] args) {
-//        ProductRepository productRepository = new ProductRepositoryImpl();
-//        ProductService productService = new ProductServiceImpl(productRepository);
-//
-//
-//        Product product = new Product("Jam", Category.FURNITURE, 30000, 5);
-//        Product product2 = new Product("Jam Tangan", Category.ELECTRONIC, 150_000, 7);
-//
-//
-//        productService.add(product);
-//        productService.add(product2);
-//        productService.findAll();
+
     }
 }
