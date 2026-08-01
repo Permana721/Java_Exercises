f2748e34526208be777099e0c86bc8c9ac4f2705
Permana <sdharma721@gmail.com>
2026-08-01 21:01:19 +0700

    refactor: remove unused methods and simplify order service interface

--- src/_04_ECommerce/service/OrderService.java
+++ src/_04_ECommerce/service/OrderService.java
@@ -1,12 +1,10 @@
 package _04_ECommerce.service;
 
 import _04_ECommerce.entity.Order;
+import _04_ECommerce.entity.User;
 
 import java.util.List;
 
 public interface OrderService {
-    String checkout(String customerId);
-    Order processNextOrder();
-    List<Order> getOrderHistory(String customerId);
-    List<Order> getAllOrders();
+    boolean checkout(User user);
 }
