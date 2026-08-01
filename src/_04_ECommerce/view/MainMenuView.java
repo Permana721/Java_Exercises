5f84758e8e06dc9a1cb83d707ada442f272078d5
Permana <sdharma721@gmail.com>
2026-08-01 15:02:16 +0700

    feat: add validation and exception handling in MainMenuView.java

--- src/_04_ECommerce/view/MainMenuView.java
+++ src/_04_ECommerce/view/MainMenuView.java
@@ -2,6 +2,7 @@ package _04_ECommerce.view;
 
 import _04_ECommerce.entity.User;
 import _04_ECommerce.entity.UserRole;
+import _04_ECommerce.exception.UserAlreadyExistsException;
 import _04_ECommerce.service.ProductService;
 import _04_ECommerce.service.UserService;
 import _04_ECommerce.util.InputUtil;
@@ -32,6 +33,7 @@ public class MainMenuView {
                 case "0" -> {
                     return;
                 }
+                default -> System.out.println("Tolong masukkan input yang valid!");
             }
         }
     }
@@ -49,11 +51,16 @@ public class MainMenuView {
             default -> System.out.println("Tolong masukan input yang valid!");
         }
 
-        if (nama == null || password == null){
+        if (nama.isBlank() || password.isBlank()){
             System.out.println("Nama atau password tidak boleh kosong!");
         } else {
-            User newUser = new User(nama, password, userRole);
-            userService.createUser(newUser);
+            try {
+                User newUser = new User(nama, password, userRole);
+                userService.createUser(newUser);
+                System.out.println("User dengan nama: " + newUser.getName() + " Berhasil dibuat!");
+            } catch (UserAlreadyExistsException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
@@ -62,12 +69,17 @@ public class MainMenuView {
         var nama = InputUtil.input("Masukkan nama anda: ");
         var password = InputUtil.input("Masukkan password anda: ");
 
-        User currentUser = userService.login(nama, password);
-        if (currentUser.getUserRole().equals(UserRole.ADMIN)){
-            AdminView adminView = new AdminView(productService);
-            adminView.showMenu();
+        if (nama.isBlank() || password.isBlank()){
+            System.out.println("Nama atau password tidak boleh kosong!");
         } else {
-            CustomerView customerView = new CustomerView();
+            User currentUser = userService.login(nama, password);
+            if (currentUser.getUserRole().equals(UserRole.ADMIN)){
+                AdminView adminView = new AdminView(productService, userService);
+                adminView.showMenu();
+            } else {
+                CustomerView customerView = new CustomerView(userService, productService, currentUser);
+                customerView.showMenu();
+            }
         }
     }
 }
