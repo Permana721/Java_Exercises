5042c4c67ed6a3ded8b612a33681fedc87c4f81e
Permana <sdharma721@gmail.com>
2026-08-01 15:02:16 +0700

    refactor: trim input string in InputUtil.java

--- src/_04_ECommerce/util/InputUtil.java
+++ src/_04_ECommerce/util/InputUtil.java
@@ -7,21 +7,6 @@ public class InputUtil {
 
     public static String input(String info) {
         System.out.print(info + " ");
-        String data = scanner.nextLine();
-        return data;
-    }
-
-    public static double aDouble(String info){
-        System.out.println(info + " ");
-        double data = scanner.nextDouble();
-        scanner.nextLine();
-        return data;
-    }
-
-    public static int anInt(String info){
-        System.out.println(info + " ");
-        int data = scanner.nextInt();
-        scanner.nextLine();
-        return data;
+        return scanner.nextLine().trim();
     }
 }
