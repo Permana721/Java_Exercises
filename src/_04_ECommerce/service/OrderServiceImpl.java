a3de3dc23ee1ed75b5b6c325a83807654c6ee780
Permana <sdharma721@gmail.com>
2026-08-02 16:44:12 +0700

    refactor: add validation and processing logic in OrderServiceImpl.java

--- src/_04_ECommerce/service/OrderServiceImpl.java
+++ src/_04_ECommerce/service/OrderServiceImpl.java
@@ -1,21 +1,41 @@
 package _04_ECommerce.service;
 
+import _04_ECommerce.entity.CartItem;
 import _04_ECommerce.entity.Order;
+import _04_ECommerce.entity.OrderItem;
 import _04_ECommerce.entity.User;
+import _04_ECommerce.exception.PaymentFailedException;
+import _04_ECommerce.exception.ProductNotFoundException;
 
 import java.util.List;
 import java.util.Objects;
 
 public class OrderServiceImpl implements OrderService {
     @Override
-    public boolean checkout(User user) {
-        if (Objects.isNull(user.getCart())){
-            return false;
+    public void checkout(User user) {
+        List<CartItem> cartItems = user.getCart().getCartItems();
+        if (user.getCart().getCartItems().isEmpty()){
+            throw new ProductNotFoundException("Anda tidak memiliki barang belanja di dalam keranjang anda!");
         }
         double total = user.getCart().calculateTotal();
         if (user.getSaldo() >= total) {
+            for (CartItem cartItem : cartItems) {
+                if (cartItem.getProduct().getStock() < cartItem.getQuantity()) {
+                    throw new PaymentFailedException("Stok tidak cukup!");
+                }
+            }
+            Order order = new Order();
+            for (CartItem cartItem : cartItems) {
+                OrderItem orderItem = new OrderItem(cartItem.getProduct(), cartItem.getQuantity());
 
+                order.addOrderItem(orderItem);
+                orderItem.getProduct().decreaseStock(cartItem.getQuantity());
+            }
+            user.withdraw(total);
+            user.getCart().clear();
+            user.getDetailUser();
+        } else {
+            throw new PaymentFailedException("Saldo anda tidak cukup!");
         }
-        return true;
     }
 }
