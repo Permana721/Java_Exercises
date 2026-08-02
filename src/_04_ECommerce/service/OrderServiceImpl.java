73138ade1b4672b4b031656703a5eb79af9b843e
Permana <sdharma721@gmail.com>
2026-08-02 10:07:50 +0700

    refactor: add null check and payment logic in OrderServiceImpl

--- src/_04_ECommerce/service/OrderServiceImpl.java
+++ src/_04_ECommerce/service/OrderServiceImpl.java
@@ -4,10 +4,18 @@ import _04_ECommerce.entity.Order;
 import _04_ECommerce.entity.User;
 
 import java.util.List;
+import java.util.Objects;
 
 public class OrderServiceImpl implements OrderService {
     @Override
     public boolean checkout(User user) {
-        return false;
+        if (Objects.isNull(user.getCart())){
+            return false;
+        }
+        double total = user.getCart().calculateTotal();
+        if (user.getSaldo() >= total) {
+
+        }
+        return true;
     }
 }
