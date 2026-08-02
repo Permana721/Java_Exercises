c82a822af0776a1664eaa80f04dafe585ced27ea
Permana <sdharma721@gmail.com>
2026-08-02 16:44:12 +0700

    refactor: import and instantiate OrderService in TestUser

--- src/_04_ECommerce/test/TestUser.java
+++ src/_04_ECommerce/test/TestUser.java
@@ -5,19 +5,17 @@ import _04_ECommerce.repository.ProductRepository;
 import _04_ECommerce.repository.ProductRepositoryImpl;
 import _04_ECommerce.repository.UserRepository;
 import _04_ECommerce.repository.UserRepositoryImpl;
-import _04_ECommerce.service.ProductService;
-import _04_ECommerce.service.ProductServiceImpl;
-import _04_ECommerce.service.UserService;
-import _04_ECommerce.service.UserServiceImpl;
+import _04_ECommerce.service.*;
 import _04_ECommerce.view.MainMenuView;
 
 public class TestUser {
     static void main(String[] args) {
         UserRepository userRepository = new UserRepositoryImpl();
         UserService userService = new UserServiceImpl(userRepository);
+        OrderService orderService = new OrderServiceImpl();
         ProductRepositoryImpl productRepository = new ProductRepositoryImpl();
         ProductServiceImpl productService = new ProductServiceImpl(productRepository);
-        MainMenuView mainMenuView = new MainMenuView(userService, productService);
+        MainMenuView mainMenuView = new MainMenuView(userService, productService, orderService);
 
         mainMenuView.showMainMenu();
     }
