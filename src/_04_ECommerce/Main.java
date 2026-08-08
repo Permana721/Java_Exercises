f77fbfa1a5a8f16f66009b143f37e2009df09bb5
Permana <sdharma721@gmail.com>
2026-08-08 20:10:18 +0700

    refactor: move and rename repositories and services in Main.java

--- src/_04_ECommerce/Main.java
+++ src/_04_ECommerce/Main.java
@@ -1,11 +1,20 @@
 package _04_ECommerce;
 
+import _04_ECommerce.repository.ProductRepositoryImpl;
+import _04_ECommerce.repository.UserRepository;
+import _04_ECommerce.repository.UserRepositoryImpl;
+import _04_ECommerce.service.*;
+import _04_ECommerce.view.MainMenuView;
+
 public class Main {
     static void main(String[] args) {
-        int a = 0;
-        Integer b = null;
+        UserRepository userRepository = new UserRepositoryImpl();
+        UserService userService = new UserServiceImpl(userRepository);
+        OrderService orderService = new OrderServiceImpl();
+        ProductRepositoryImpl productRepository = new ProductRepositoryImpl();
+        ProductServiceImpl productService = new ProductServiceImpl(productRepository);
+        MainMenuView mainMenuView = new MainMenuView(userService, productService, orderService);
 
-        System.out.println(a);
-        System.out.println(b);
+        mainMenuView.showMainMenu();
     }
 }
