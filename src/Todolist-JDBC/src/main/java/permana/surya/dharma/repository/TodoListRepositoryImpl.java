7dabb8ad9ce8b7ec564503275202786c9d6a9fe5
Permana <sdharma721@gmail.com>
2026-08-19 17:25:37 +0700

    feat: add TodoListRepositoryImpl implementation

--- src/Todolist-JDBC/src/main/java/permana/surya/dharma/repository/TodoListRepositoryImpl.java
+++ src/Todolist-JDBC/src/main/java/permana/surya/dharma/repository/TodoListRepositoryImpl.java
@@ -1,4 +1,44 @@
 package permana.surya.dharma.repository;
 
+import permana.surya.dharma.entity.Todolist;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+
 public class TodoListRepositoryImpl implements TodoListRepository {
+    private final List<Todolist> todolistData;
+
+    public TodoListRepositoryImpl() {
+        this.todolistData = new ArrayList<>();
+    }
+
+    @Override
+    public Todolist save(Todolist todolist) {
+        todolistData.add(todolist);
+        return todolist;
+    }
+
+    @Override
+    public Todolist findById(Integer id) {
+        for (Todolist todolist : todolistData) {
+            if (todolist.getId().equals(id)) {
+                return todolist;
+            }
+        }
+        throw new InputMismatchException("Todolist dengan nomor " + id + " tidak ditemukan!");
+    }
+
+    @Override
+    public Todolist delete(Integer id) {
+        Todolist todolist = findById(id);
+        todolistData.remove(todolist);
+        System.out.println("Todolist " + todolist.getTodo() + " berhasil dihapus!");
+        return todolist;
+    }
+
+    @Override
+    public List<Todolist> findAll() {
+        return todolistData;
+    }
 }
