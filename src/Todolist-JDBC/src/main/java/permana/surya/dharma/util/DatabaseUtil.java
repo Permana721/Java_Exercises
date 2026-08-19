6a31b2baf98a60e7f65e297b98eee83820362bdb
Permana <sdharma721@gmail.com>
2026-08-19 17:25:37 +0700

    feat: add DatabaseUtil class for managing JDBC connections

--- src/Todolist-JDBC/src/main/java/permana/surya/dharma/util/DatabaseUtil.java
+++ src/Todolist-JDBC/src/main/java/permana/surya/dharma/util/DatabaseUtil.java
@@ -1,4 +1,29 @@
 package permana.surya.dharma.util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class DatabaseUtil {
+    private static HikariDataSource hikariDataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.postgresql.Driver");
+        config.setUsername("permana");
+        config.setPassword("");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/java");
+
+        // pool
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(5);
+        config.setIdleTimeout(60_000);
+        config.setMaxLifetime(60 * 60_000);
+        config.setConnectionTimeout(30_000);
+
+        hikariDataSource = new HikariDataSource(config);
+    }
+
+    public static HikariDataSource getDataSource() {
+        return hikariDataSource;
+    }
 }
