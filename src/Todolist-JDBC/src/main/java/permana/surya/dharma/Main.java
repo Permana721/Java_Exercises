f7418e186050722c2f29af550318cae798e9c85d
Permana <sdharma721@gmail.com>
2026-08-19 17:25:37 +0700

    refactor: add dependency injection and service layer

--- src/Todolist-JDBC/src/main/java/permana/surya/dharma/Main.java
+++ src/Todolist-JDBC/src/main/java/permana/surya/dharma/Main.java
@@ -1,4 +1,17 @@
 package permana.surya.dharma;
 
+import permana.surya.dharma.repository.TodoListRepository;
+import permana.surya.dharma.repository.TodoListRepositoryImpl;
+import permana.surya.dharma.service.TodoListService;
+import permana.surya.dharma.service.TodoListServiceImpl;
+import permana.surya.dharma.view.TodoListView;
+
 public class Main {
+    static void main(String[] args) {
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListView.showTodoList();
+    }
 }
