93102ace564296ea6c81c141b898ea3c53c6d934
Permana <sdharma721@gmail.com>
2026-08-19 17:25:37 +0700

    feat: add CRUD methods to TodoListRepository

--- src/Todolist-JDBC/src/main/java/permana/surya/dharma/repository/TodoListRepository.java
+++ src/Todolist-JDBC/src/main/java/permana/surya/dharma/repository/TodoListRepository.java
@@ -1,4 +1,12 @@
 package permana.surya.dharma.repository;
 
+import permana.surya.dharma.entity.Todolist;
+
+import java.util.List;
+
 public interface TodoListRepository {
+    Todolist save(Todolist todolist);
+    Todolist findById(Integer id);
+    Todolist delete(Integer id);
+    List<Todolist> findAll();
 }
