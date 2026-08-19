5d9b079c467db9bf1e136b1906cd676aa978600e
Permana <sdharma721@gmail.com>
2026-08-19 17:25:37 +0700

    refactor: add TodoListView functionality and dependencies

--- src/Todolist-JDBC/src/main/java/permana/surya/dharma/view/TodoListView.java
+++ src/Todolist-JDBC/src/main/java/permana/surya/dharma/view/TodoListView.java
@@ -1,5 +1,69 @@
 package permana.surya.dharma.view;
 
-public class TodoListView {
+import permana.surya.dharma.entity.Todolist;
+import permana.surya.dharma.service.TodoListService;
+import permana.surya.dharma.util.InputUtil;
 
+import java.util.List;
+
+public class TodoListView {
+    private TodoListService todoListService;
+
+    public TodoListView(TodoListService todoListService) {
+        this.todoListService = todoListService;
+        todolistList = todoListService.findAll();
+    }
+
+    List<Todolist> todolistList;
+
+
+    public void showTodoList(){
+        while (true) {
+            for (Todolist todo : todolistList) {
+                System.out.println(todo.getId() + ". " + todo.getTodo());
+            }
+
+            System.out.println("MENU: ");
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("0. Keluar");
+
+            var input = InputUtil.input("Pilih: ");
+
+            if (input.equals("1")){
+                addTodoList();
+            } else if (input.equals("2")) {
+                removeTodoList();
+            } else if (input.equals("0")) {
+                break;
+            } else {
+                System.out.println("Input tidak valid!");
+            }
+        }
+    }
+
+    public void addTodoList(){
+        System.out.println("MENAMBAH TODOLIST");
+
+        var todo = InputUtil.input("Todo: ");
+
+        if (todo.equals("0")) {
+            return;
+        } else {
+            Todolist todolist = new Todolist(todo);
+            todoListService.addTodoList(todolist);
+        }
+    }
+
+    public void removeTodoList(){
+        System.out.println("MENGHAPUS TODOLIST");
+
+        var number = InputUtil.input("Nomor yang dihapus: ");
+
+        if (number.equals("0")) {
+            return;
+        } else {
+            todoListService.removeTodoList(Integer.valueOf(number));
+        }
+    }
 }
