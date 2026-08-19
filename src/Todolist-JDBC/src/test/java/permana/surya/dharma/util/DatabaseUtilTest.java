28ea71ee2d887c0c03b3c8eff365f77cdffe0e8d
Permana <sdharma721@gmail.com>
2026-08-19 17:25:37 +0700

    feat: add DatabaseUtilTest for JDBC connection testing

--- src/Todolist-JDBC/src/test/java/permana/surya/dharma/util/DatabaseUtilTest.java
+++ src/Todolist-JDBC/src/test/java/permana/surya/dharma/util/DatabaseUtilTest.java
@@ -1,4 +1,19 @@
 package permana.surya.dharma.util;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DatabaseUtilTest {
+    @Test
+    void testConnection() throws SQLException {
+        HikariDataSource dataSource = DatabaseUtil.getDataSource();
+
+        Connection connection = dataSource.getConnection();
+
+        connection.close();
+        dataSource.close();
+    }
 }
