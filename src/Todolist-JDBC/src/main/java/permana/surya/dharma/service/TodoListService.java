e4a9e0de852f214485d0f03408b773aa1fc5e868
Permana <sdharma721@gmail.com>
2026-08-19 17:25:37 +0700

    feat: add CRUD operations to TodoListService

--- src/Todolist-JDBC/src/main/java/permana/surya/dharma/service/TodoListService.java
+++ src/Todolist-JDBC/src/main/java/permana/surya/dharma/service/TodoListService.java
@@ -1,4 +1,11 @@
 package permana.surya.dharma.service;
 
+import permana.surya.dharma.entity.Todolist;
+
+import java.util.List;
+
 public interface TodoListService {
+    Todolist addTodoList(Todolist todo);
+    Todolist removeTodoList(Integer id);
+    List<Todolist> findAll();
 }
