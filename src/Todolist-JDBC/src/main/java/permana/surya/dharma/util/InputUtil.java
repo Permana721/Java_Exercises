5657b489e2c4632b913185a1a8663f6e1706b0ea
Permana <sdharma721@gmail.com>
2026-08-19 17:25:37 +0700

    feat: add input functionality to InputUtil class

--- src/Todolist-JDBC/src/main/java/permana/surya/dharma/util/InputUtil.java
+++ src/Todolist-JDBC/src/main/java/permana/surya/dharma/util/InputUtil.java
@@ -1,4 +1,13 @@
 package permana.surya.dharma.util;
 
+import java.util.Scanner;
+
 public class InputUtil {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String input(String info) {
+        System.out.print(info + " ");
+        String data = scanner.nextLine();
+        return data;
+    }
 }
