8b00a497b67bc24744777adaa3d5993c96df0199
Permana <sdharma721@gmail.com>
2026-08-19 17:25:37 +0700

    chore: update Todolist.java

--- src/Todolist-JDBC/src/main/java/permana/surya/dharma/entity/Todolist.java
+++ src/Todolist-JDBC/src/main/java/permana/surya/dharma/entity/Todolist.java
@@ -1,5 +1,32 @@
 package permana.surya.dharma.entity;
 
 public class Todolist {
-    
+    private String todo;
+    private Integer id;
+    private static int counter = 1;
+
+    public Todolist(String todo) {
+        this.todo = todo;
+        setId(increaseID());
+    }
+
+    public static int increaseID() {
+        return counter++;
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
