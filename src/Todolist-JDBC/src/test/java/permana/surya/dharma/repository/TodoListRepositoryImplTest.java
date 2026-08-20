4ed4468155237a566abd6cbe8c357c571107da62
Permana <sdharma721@gmail.com>
2026-08-20 13:33:20 +0700

    feat: add TodoListRepositoryImplTest for JDBC repository tests

--- src/Todolist-JDBC/src/test/java/permana/surya/dharma/repository/TodoListRepositoryImplTest.java
+++ src/Todolist-JDBC/src/test/java/permana/surya/dharma/repository/TodoListRepositoryImplTest.java
@@ -1,4 +1,49 @@
 package permana.surya.dharma.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import permana.surya.dharma.entity.Todolist;
+import permana.surya.dharma.util.DatabaseUtil;
+
+import java.util.List;
+
 public class TodoListRepositoryImplTest {
+    private HikariDataSource dataSource;
+    private TodoListRepository todoListRepository;
+
+    @BeforeEach
+    void setUp() {
+        dataSource = DatabaseUtil.getDataSource();
+        todoListRepository = new TodoListRepositoryImpl(dataSource);
+    }
+
+    @Test
+    void testAdd() {
+        todoListRepository.save(new Todolist("Cihuy"));
+        todoListRepository.save(new Todolist("Prikitiw"));
+        todoListRepository.save(new Todolist("Seleketep"));
+        todoListRepository.save(new Todolist("Satu dua"));
+    }
+
+    @Test
+    void testRemove() {
+        System.out.println(todoListRepository.delete(1));
+        System.out.println(todoListRepository.delete(2));
+        System.out.println(todoListRepository.delete(3));
+    }
+
+    @Test
+    void testFindAll() {
+        List<Todolist> todolists = todoListRepository.findAll();
+        for (Todolist todolist : todolists) {
+            System.out.println(todolist.getId() + ". " + todolist.getTodo());
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        dataSource.close();
+    }
 }
