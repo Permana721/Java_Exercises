f9441151141ba41965f152698d39ab14e449ccb3
Permana <sdharma721@gmail.com>
2026-08-20 13:33:20 +0700

    refactor: inject dataSource into TodoListRepositoryImpl

--- src/Todolist-JDBC/src/main/java/permana/surya/dharma/Main.java
+++ src/Todolist-JDBC/src/main/java/permana/surya/dharma/Main.java
@@ -4,11 +4,15 @@ import permana.surya.dharma.repository.TodoListRepository;
 import permana.surya.dharma.repository.TodoListRepositoryImpl;
 import permana.surya.dharma.service.TodoListService;
 import permana.surya.dharma.service.TodoListServiceImpl;
+import permana.surya.dharma.util.DatabaseUtil;
 import permana.surya.dharma.view.TodoListView;
+
+import javax.sql.DataSource;
 
 public class Main {
     static void main(String[] args) {
-        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        DataSource dataSource = DatabaseUtil.getDataSource();
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl(dataSource);
         TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
         TodoListView todoListView = new TodoListView(todoListService);
 
