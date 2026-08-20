8350910c2fd0197ced2a42aef4479d58bbc71d83
Permana <sdharma721@gmail.com>
2026-08-20 13:33:20 +0700

    feat: refactor TodoListRepositoryImpl to use JDBC with DataSource

--- src/Todolist-JDBC/src/main/java/permana/surya/dharma/repository/TodoListRepositoryImpl.java
+++ src/Todolist-JDBC/src/main/java/permana/surya/dharma/repository/TodoListRepositoryImpl.java
@@ -2,43 +2,98 @@ package permana.surya.dharma.repository;
 
 import permana.surya.dharma.entity.Todolist;
 
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 
 public class TodoListRepositoryImpl implements TodoListRepository {
-    private final List<Todolist> todolistData;
+    private DataSource dataSource;
 
-    public TodoListRepositoryImpl() {
-        this.todolistData = new ArrayList<>();
+    public TodoListRepositoryImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Override
     public Todolist save(Todolist todolist) {
-        todolistData.add(todolist);
+        String sql = "INSERT INTO todolist(todo) VALUES (?)";
+
+        try(Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, todolist.getTodo());
+            statement.executeUpdate();
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
         return todolist;
     }
 
     @Override
     public Todolist findById(Integer id) {
-        for (Todolist todolist : todolistData) {
-            if (todolist.getId().equals(id)) {
-                return todolist;
-            }
-        }
+//        for (Todolist todolist : todolistData) {
+//            if (todolist.getId().equals(id)) {
+//                return todolist;
+//            }
+//        }
         throw new InputMismatchException("Todolist dengan nomor " + id + " tidak ditemukan!");
     }
 
+    private boolean isExist(Integer id) {
+        String sql = "SELECT id FROM todolist WHERE id = ?";
+        try(Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            try(ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
+
+    }
+
     @Override
-    public Todolist delete(Integer id) {
-        Todolist todolist = findById(id);
-        todolistData.remove(todolist);
-        System.out.println("Todolist " + todolist.getTodo() + " berhasil dihapus!");
-        return todolist;
+    public boolean delete(Integer id) {
+        if(isExist(id)) {
+            String sql = "DELETE FROM todolist WHERE id = ?";
+            try(Connection connection = dataSource.getConnection()) {
+                PreparedStatement statement = connection.prepareStatement(sql);
+
+                statement.setInt(1, id);
+                statement.executeUpdate();
+            } catch (SQLException exception) {
+                throw new RuntimeException(exception);
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public List<Todolist> findAll() {
-        return todolistData;
+        String sql = "SELECT id, todo FROM todolist";
+        try(Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql)) {
+
+            List<Todolist> todolists = new ArrayList<>();
+            while (resultSet.next()) {
+                Todolist todolist = new Todolist();
+                todolist.setId(resultSet.getInt("id"));
+                todolist.setTodo(resultSet.getString("todo"));
+
+                todolists.add(todolist);
+            }
+            return todolists;
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 }
