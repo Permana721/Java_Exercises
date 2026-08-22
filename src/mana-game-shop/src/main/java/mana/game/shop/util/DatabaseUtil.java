03d5926dcdcbde2ebf310b69b36313ee7632b223
Permana <sdharma721@gmail.com>
2026-08-22 23:31:25 +0700

    chore: update DatabaseUtil.java

--- src/mana-game-shop/src/main/java/mana/game/shop/util/DatabaseUtil.java
+++ src/mana-game-shop/src/main/java/mana/game/shop/util/DatabaseUtil.java
@@ -1,4 +1,29 @@
 package mana.game.shop.util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class DatabaseUtil {
+    private static final HikariDataSource hikariDataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.postgresql.Driver");
+        config.setUsername("permana");
+        config.setPassword("");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/mana_game_db");
+
+        //pool
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(5);
+        config.setIdleTimeout(60_000);
+        config.setMaxLifetime(60 * 60_000);
+        config.setConnectionTimeout(30_000);
+
+        hikariDataSource = new HikariDataSource(config);
+    }
+
+    public static HikariDataSource getDataSource() {
+        return hikariDataSource;
+    }
 }
